95f35d579c212149b976f2b79e022198a3b2660c
huy811 <huydn811@gmail.com>
2021-04-18 16:39:06 +0700

    Prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "somedeployURL";
+    : "https://immense-brook-38698.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME='learnit-mern'
 
